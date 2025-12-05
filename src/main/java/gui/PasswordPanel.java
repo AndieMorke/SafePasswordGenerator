022c74fd0550b0f8f7d24bc021899f9167f579ae
Andie Morke <morkeandie@gmail.com>
2025-12-05 23:15:30 +0100

    Correcciones

--- src/main/java/gui/PasswordPanel.java
+++ src/main/java/gui/PasswordPanel.java
@@ -18,13 +18,14 @@ public class PasswordPanel extends JPanel {
 
        setLayout(new BorderLayout(20,10));
        setBorder(new EmptyBorder(10, 20, 10, 20)); // top, left, bottom, right
-       //setBackground(new Color(250,250,250));
+       setBackground(new Color(255,218,185));
 
         // PANEL PRINCIPAL NORTE - TÍTULO
         northPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         label = new JLabel("CONTRASEÑA SEGURA");
         label.setFont(new Font("Arial", Font.BOLD,26));
         northPanel.add(label);
+        northPanel.setBackground(new Color(255,218,185));
         add(northPanel,BorderLayout.NORTH);
 
         // PANEL PRINCIPAL CENTRO - MENÚ
@@ -35,13 +36,16 @@ public class PasswordPanel extends JPanel {
                         length.setFont(new Font("Arial", Font.PLAIN,16));
                         number = new JTextField(3);
                         number.setFont(new Font("Arial", Font.PLAIN,20));
+                        number.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
                     lengthPanel.add(length);
                     lengthPanel.add(number);
+                    lengthPanel.setBackground(new Color(255,218,185));
                 generatePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
                     genPasswordButton = new JButton("GENERAR");
                     genPasswordButton.setFont(new Font("Arial", Font.BOLD,16));
                     genPasswordButton.setBackground(new Color(200,200,200));
                 generatePanel.setBorder(new EmptyBorder(0, 0, 20, 0));
+                generatePanel.setBackground(new Color(255,218,185));
                 generatePanel.add(genPasswordButton);
             menuPanel.add(lengthPanel);
             menuPanel.add(generatePanel);
@@ -59,12 +63,14 @@ public class PasswordPanel extends JPanel {
                 showHiddenPasswordButton.setFont(new Font("Arial", Font.BOLD,16));
                 showHiddenPasswordButton.setBackground(new Color(200,200,200));
                 showPanel.setBorder(new EmptyBorder(20,0,0,0));
+            showPanel.setBackground(new Color(255,218,185));
             showPanel.add(showHiddenPasswordButton);
 
         centerPanel.setBorder(new EmptyBorder(10,0,10,0));
         centerPanel.add(menuPanel,BorderLayout.NORTH);
         centerPanel.add(showPassword,BorderLayout.CENTER);
         centerPanel.add(showPanel,BorderLayout.SOUTH);
+        centerPanel.setBackground(new Color(255,218,185));
 
        add(centerPanel,BorderLayout.CENTER);
 
@@ -74,6 +80,7 @@ public class PasswordPanel extends JPanel {
        southPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
        copyright = new JLabel("© Andie Mørke");
        copyright.setFont(new Font("Serif", Font.PLAIN,14));
+       southPanel.setBackground(new Color(255,218,185));
        southPanel.add(copyright);
        add(southPanel,BorderLayout.SOUTH);
 
