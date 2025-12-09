e0d2075af2774a5fd6d305a30a5160ae1d0ff08d
Andie Morke <morkeandie@gmail.com>
2025-12-09 20:20:34 +0100

    Correcciones

--- src/main/java/model/Password.java
+++ src/main/java/model/Password.java
@@ -4,27 +4,26 @@ import java.util.Random;
 
 public class Password {
 
+    /**
+     * Atributos
+     */
     private char[] lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz".toCharArray();
     private char[] numbers = "0123456789".toCharArray();
     private char[] specialCharacters = "!@#$%*()_+-=[]{}.,?".toCharArray();
     private char[] upperCaseCharacters;
-
     private char[] userPassword;
-
     private final Random RANDOM;
-
     private boolean hasLower,hasUpper,hasSpecial,hasNumber;
-
     private enum CharType {LOWER,UPPER,SPECIAL,NUMBER}
 
-
+    /**
+     * CONSTRUCTOR
+     */
     public Password() {
-
         upperCaseCharacters = new char[lowerCaseCharacters.length];
         for (int i = 0; i < lowerCaseCharacters.length; i++) {
             upperCaseCharacters[i] = Character.toUpperCase(lowerCaseCharacters[i]);
         }
-
         RANDOM = new Random();
     }
 
@@ -47,21 +46,18 @@ public class Password {
     public void setHasNumber(boolean hasNumber) {
         this.hasNumber = hasNumber;
     }
-
     public boolean hasSpecial() {
         return this.hasSpecial;
     }
     public void setHasSpecial(boolean hasSpecial) {
         this.hasSpecial = hasSpecial;
     }
-
     public boolean hasUpper() {
         return this.hasUpper;
     }
     public void setHasUpper(boolean hasUpper) {
         this.hasUpper = hasUpper;
     }
-
     public boolean hasLower() {
         return this.hasLower;
     }
@@ -76,20 +72,15 @@ public class Password {
         return this.userPassword;
     }
 
-
-    public void createUserPassword() {
-
-
-        for (int i = 0; i < this.getUserPassword().length; i++) {
-
-            do {
-                this.setRandomCharacter(i,this.getRandomCharacterType());
-            } while (this.areConsecutive(i));
-        }
-
-        this.fillMissingCharacterTypes();
+    public char[] getRandomCharacterType(){
+        CharType charType = CharType.values()[RANDOM.nextInt(CharType.values().length)];
+        return switch (charType) {
+            case LOWER -> this.getLowerCaseCharacters();
+            case UPPER -> this.getUpperCaseCharacters();
+            case SPECIAL -> this.getSpecialCharacters();
+            case NUMBER -> this.getNumbers();
+        };
     }
-
     public void setRandomIndexAndRandomCharacter(char[] charArray) {
         int randomIndex;
         char randomChar;
@@ -100,33 +91,19 @@ public class Password {
                 || (randomIndex < userPassword.length - 1 && this.userPassword[randomIndex + 1] == randomChar));
         this.userPassword[randomIndex] = randomChar;
     }
-
-    public char[] getRandomCharacterType(){
-        CharType charType = CharType.values()[RANDOM.nextInt(CharType.values().length)];
-        return switch (charType) {
-            case LOWER -> this.getLowerCaseCharacters();
-            case UPPER -> this.getUpperCaseCharacters();
-            case SPECIAL -> this.getSpecialCharacters();
-            case NUMBER -> this.getNumbers();
-        };
-    }
-
-
-    public char getRandomCharacter(char[] array){
-        return array[RANDOM.nextInt(array.length)];
+    public int getRandomIndex(){
+        return RANDOM.nextInt(this.getUserPassword().length);
     }
     public void setRandomCharacter(int index,char[] array){
         this.userPassword[index] = this.getRandomCharacter(array);
     }
-
-    public int getRandomIndex(){
-        return RANDOM.nextInt(this.getUserPassword().length);
+    public char getRandomCharacter(char[] array){
+        return array[RANDOM.nextInt(array.length)];
     }
 
     public boolean areConsecutive(int position){
         return ((position > 0) && (this.getUserPassword()[position] == this.getUserPassword()[position - 1]));
     }
-
     public void fillMissingCharacterTypes(){
         while(!this.hasUpper() || !this.hasLower() || !this.hasSpecial() || !this.hasNumber()){
             if(!this.hasUpper()){
@@ -146,5 +123,17 @@ public class Password {
                 this.setHasLower(true);
             }
         }
+    }
+
+    public void createUserPassword() {
+
+        for (int i = 0; i < this.getUserPassword().length; i++) {
+
+            do {
+                this.setRandomCharacter(i,this.getRandomCharacterType());
+            } while (this.areConsecutive(i));
+        }
+
+        this.fillMissingCharacterTypes();
     }
 }
